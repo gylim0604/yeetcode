e6383fd1c528db1cedbfbbfba5e97dfdaf1fa750
gylim0604 <gylim0604@hotmail.com>
2024-12-13 15:04:30 +0800

    add solution for find score using single pass

--- dailies/findScore.py
+++ dailies/findScore.py
@@ -2,7 +2,8 @@ import heapq
 
 
 class Solution:
-    def findScore(self, nums: list[int]) -> int:
+    # Solution using heap and set
+    def findScore1(self, nums: list[int]) -> int:
         score = 0
         heap = []
         marked = set()
@@ -15,5 +16,18 @@ class Solution:
                 marked.add(idx+1)
                 marked.add(idx-1)
         return score
+    def findScore(self, nums: list[int]) -> int:
+        score = 0
+        i =0
+        while i < len(nums):
+            start  = i
+            while i + 1 < len(nums) and nums[i+1] < nums[i]:
+                i+=1
+            for j in range(i, start -1, -2):
+                score+=nums[j]
+            i+=2
+
+        return score
+
 sol = Solution()
 print(sol.findScore([2,1,3,4,5,2]))
