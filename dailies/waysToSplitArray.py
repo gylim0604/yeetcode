fef4e0da4878571e8d513b13632a22276f5fe99c
gylim0604 <gylim0604@hotmail.com>
2025-01-03 16:01:53 +0800

    add optimized solution for ways to split array

--- dailies/waysToSplitArray.py
+++ dailies/waysToSplitArray.py
@@ -1,14 +1,11 @@
 class Solution:
     def waysToSplitArray(self, nums: list[int]) -> int:
         res = 0
-        prefixSum = [0] * (len(nums) + 1)
-        suffixSum = [0] * (len(nums) + 1)
-        for i in range(1, len(prefixSum)):
-            prefixSum[i] = prefixSum[i-1] + nums[i-1]
-        for i in range(len(prefixSum) - 2, -1, -1):
-            suffixSum[i] = suffixSum[i+1] + nums[i]
-        for i in range(1, len(prefixSum) -1):
-            left, right = prefixSum[i], suffixSum[i]
+        left = 0
+        totalSum = sum(nums)
+        for _, val in enumerate(nums[:-1]):
+            left += val
+            right = totalSum - left
             if left >= right:
                 res+=1
         return res
