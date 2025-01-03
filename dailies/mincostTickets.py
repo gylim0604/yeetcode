54e164ee42832834ba55675f5cb3b924e63dd33a
gylim0604 <gylim0604@hotmail.com>
2025-01-03 15:29:07 +0800

    add solution for min cost tickets

--- dailies/mincostTickets.py
+++ dailies/mincostTickets.py
@@ -1,21 +1,22 @@
 class Solution:
     def mincostTickets(self, days: list[int], costs: list[int]) -> int:
-        s = set(days)
-        maxDay = days[-1]
-        dp = [0] * (maxDay + 1)
+        
+        dp = [0] * len(days)
+        dp[0] = costs[0]
+        for i in range(len(days)):
+            cost_1 = (dp[i-1] + costs[0]) if i > 0 else costs[0]
+            j = i
+            while j >= 0 and days[j] >= days[i] - 6:
+                j-=1
+            cost_7 = dp[j]+ costs[1] if j >= 0 else costs[1]
+            j = i
+            while j >= 0 and days[j] >= days[i] - 29:
+                j-=1
+            cost_30 = dp[j]+ costs[2] if j >= 0 else costs[2]
+            
+            dp[i] = min(cost_1, cost_7, cost_30)
 
-        for i in range(1,len(dp)):
-            # is a travel day
-            if i in s:
-                cost_1 = dp[i - 1] + costs[0]
-                cost_7 = dp[max(0,i-7)] + costs[1]
-                cost_30 = dp[max(0,i-30)] + costs[2] 
-                print(cost_1, cost_7,cost_30)
-                dp[i] = min(cost_1, cost_7, cost_30)
-            # not a travel day
-            else:
-                dp[i] = dp[i-1]
-        return dp[maxDay]
+        return dp[-1]
 
 sol = Solution()
-print(sol.mincostTickets([1,4,6,7,8,20],[2,7,15]))
+print(sol.mincostTickets([2,33],[2,7,15]))
