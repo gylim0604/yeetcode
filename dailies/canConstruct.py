0163a93578b6376e19998276cf59b5d826f40980
gylim0604 <gylim0604@hotmail.com>
2025-01-11 15:48:44 +0800

    add can construct solution using counter

--- dailies/canConstruct.py
+++ dailies/canConstruct.py
@@ -1,16 +1,17 @@
+from typing import Counter
+
+
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        if len(s) < k:
+        s_l = len(s)
+        if s_l < k:
             return False
-        # O(1) space
-        freq = [0]*26
-        for c in s:
-            freq[ord(c)-ord('a')]+=1 
-        oddCount = 0
-        for count in freq:
-            if count%2 != 0:
-                oddCount+=1
-        return oddCount <= k
+        if s_l == k:
+            return True
+        freq = Counter(s)
+        odd_count = sum(v & 1 for v in freq.values())
+        return odd_count < k
+
 
 sol = Solution()
 print(sol.canConstruct("annabelle",2))
