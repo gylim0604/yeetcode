cd809cc62d5960e20d01936dbcf867edbd178f04
gylim0604 <gylim0604@hotmail.com>
2025-01-27 15:12:41 +0800

    add solution for check course prereq

--- dailies/checkIfPrerequisite.py
+++ dailies/checkIfPrerequisite.py
@@ -1,6 +1,31 @@
+from collections import defaultdict
+
+
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: list[list[int]], queries: list[list[int]]) -> list[bool]:
-        res = [False]*numCourses
+        adj = defaultdict(list)
+        for prereq, crs in prerequisites:
+            adj[crs].append(prereq)
+
+        prereq_map = {}
+
+        def dfs(crs):
+            if crs not in prereq_map:
+                prereq_map[crs] = set()
+                for prereq in adj[crs]:
+                    prereq_map[crs] |=dfs(prereq)
+                prereq_map[crs].add(crs)
+            return prereq_map[crs]
+        
+        for crs in range(numCourses):
+            dfs(crs)
+        res = []
+        for u, v in queries:
+            res.append(u in prereq_map[v])
+        return res
+
+
+    def floydWarshall(self, numCourses: int, prerequisites: list[list[int]], queries: list[list[int]]) -> list[bool]:
         isReachable = [[False]* numCourses for _ in range(numCourses)]
         # reachability
         for u, v in prerequisites:
